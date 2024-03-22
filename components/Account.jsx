88342c48dc869154ae34e229325bca4cffae5917
jose <sambonij346@gmail.com>
2024-03-22 12:23:24 -0400

    css jose

--- components/Account.jsx
+++ components/Account.jsx
@@ -29,63 +29,79 @@ export default function Account() {
   };
 
   return (
-    <div className="container mx-auto px-4 py-8">
-      <div className="max-w-md mx-auto mt-8 bg-white shadow-md rounded-lg p-6">
-        {users && (
-          <div>
-            <h2 className="text-center text-2xl font-bold mb-4">
-              Account Details
-            </h2>
-            <hr className="my-4" />
-            <div className="mb-4">
-              <h4 className="text-lg mb-2">Id: {users.id}</h4>
-              <h4 className="text-lg mb-2">First Name: {users.firstname}</h4>
-              <h4 className="text-lg mb-2">Last Name: {users.lastname}</h4>
-              <h4 className="text-lg mb-2">Email: {users.email}</h4>
-            </div>
-            <div className="mt-4 flex flex-col items-center justify-center">
-              <button
-                className="relative bottom-4 left-4 hover:bg-blue hover:text-white bg-transparent border border-black rounded-md px-3 py-1 transition duration-300 ease-in-out"
-                onClick={onEdit}
-              >
-                Edit
-              </button>
-            </div>
-          </div>
-        )}
-      </div>
-      <hr className="my-8" />
-      <div>
-        <h1 className="mt-4 flex flex-col items-center justify-center text-2xl font-bold mb-4">
-          Order History
-        </h1>
-        <div className="mt-10 container mx-auto grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
-          {newOrder.map((order, index) => {
-            return (
-              <div
-                key={index}
-                className="bg-white shadow-md rounded-lg overflow-hidden transition-transform transform hover:scale-105 mb-8 max-w-md w-full mx-4 md:mx-auto p-8"
-              >
-                <div className="mb-2 font-bold">Order Number: {order.id}</div>
-                <div className="mb-2">
-                  Order Placed:{" "}
-                  {order.createdat.slice(0, order.createdat.search("T"))}
-                </div>
-                <div className="mb-2">Total Price: ${order.total}</div>
-                <div className="flex flex-wrap">
-                  {order.productInfo.map((product, index) => (
-                    <div key={index} className="mb-2 flex flex-wrap mr-4">
-                      <img
-                        src={product.productDescription.image}
-                        alt={product.productDescription.title}
-                        className="h-40 w-4"
-                      />
-                    </div>
-                  ))}
-                </div>
+    <div
+      style={{
+        backgroundImage: `url('https://i.pinimg.com/originals/42/b9/01/42b901d96c8588f3aa95f1492a3648ae.png')`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        minHeight: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div className="container mx-auto px-4 py-8">
+        <div
+          className="max-w-md mx-auto mt-8 bg-white  rounded-lg p-6"
+          style={{ opacity: 0.9 }}
+        >
+          {users && (
+            <div>
+              <h2 className="text-center text-2xl font-bold mb-4">
+                Account Details
+              </h2>
+              <hr className="my-4" />
+              <div className="mb-4">
+                <h4 className="text-lg mb-2">Id: {users.id}</h4>
+                <h4 className="text-lg mb-2">First Name: {users.firstname}</h4>
+                <h4 className="text-lg mb-2">Last Name: {users.lastname}</h4>
+                <h4 className="text-lg mb-2">Email: {users.email}</h4>
               </div>
-            );
-          })}
+              <div className="mt-4 flex flex-col items-center justify-center">
+                <button
+                  className="relative bottom-4 left-4 hover:bg-blue hover:text-white bg-transparent border border-black rounded-md px-3 py-1 transition duration-300 ease-in-out"
+                  onClick={onEdit}
+                >
+                  Edit
+                </button>
+              </div>
+            </div>
+          )}
+        </div>
+        <hr className="my-8" />
+        <div>
+          <h1 className="mt-4 flex flex-col items-center justify-center text-2xl font-bold mb-4">
+            Order History
+          </h1>
+          <div className="mt-10 container mx-auto grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+            {newOrder.map((order, index) => {
+              return (
+                <div
+                  key={index}
+                  className="bg-white shadow-md rounded-lg overflow-hidden transition-transform transform hover:scale-105 mb-8 max-w-md w-full mx-4 md:mx-auto p-8"
+                  style={{ opacity: 0.9 }}
+                >
+                  <div className="mb-2 font-bold">Order Number: {order.id}</div>
+                  <div className="mb-2">
+                    Order Placed:{" "}
+                    {order.createdat.slice(0, order.createdat.search("T"))}
+                  </div>
+                  <div className="mb-2">Total Price: ${order.total}</div>
+                  <div className="flex flex-wrap">
+                    {order.productInfo.map((product, index) => (
+                      <div key={index} className="mb-2 flex flex-wrap mr-4">
+                        <img
+                          src={product.productDescription.image}
+                          alt={product.productDescription.title}
+                          className="h-40 w-4"
+                        />
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
